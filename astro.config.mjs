2454ff3443a00d03984a97b321da7505fbb21941
Bhaskar <bhaskar.sai12@gmail.com>
2026-02-28 14:47:47 -0800

    Format astro.config.mjs (quote style)

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,16 +10,16 @@
  *
  * @see https://astro.build/config
  */
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bhaskarkotakonda.github.io',
-  output: 'static',
+  site: "https://bhaskarkotakonda.github.io",
+  output: "static",
   integrations: [
     react(),
     tailwind({
@@ -30,13 +30,13 @@ export default defineConfig({
   ],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
+      theme: "github-dark",
       wrap: true,
     },
   },
   vite: {
     ssr: {
-      noExternal: ['react-icons'],
+      noExternal: ["react-icons"],
     },
   },
 });
